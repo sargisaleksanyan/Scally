ec87ba7964d369296a43f05e5d455ce7b07f326f
Sargis <sarqisjob2012@gmail.dom>
2017-09-10 22:44:07 +0400

    N

---
+++ routes/submit.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const mongo=require('mongodb').MongoClient;
+let database;
+const url="mongodb://pyotr:shaurma@ds133582.mlab.com:33582/scally";
+router.post('/submit',function(req,response){
+    let userMail=req.body.email;
+    let email={mail:userMail};
+    database.collection("user").insertOne({mail:userMail},function (err,res) {
+        if(err){
+            var error= err.statusCode;
+            var status=err.status;
+            console.log(err);
+        }
+        else{
+            response.redirect('/success');
+        }
+    })
+});
+module.exports = router;
