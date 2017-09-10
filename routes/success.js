e04731048cfbfbe5df069cd92d2d9cc2c5c9c9d2
Sargis <sarqisjob2012@gmail.dom>
2017-09-10 22:49:50 +0400

    NN

--- routes/success.js
+++ routes/success.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-app.get('/success',function (req,res) {
+router.get('/success',function (req,res) {
     res.sendFile(path.join(__dirname,'./public',"thankyou.html"));
 })
 module.exports = router;
